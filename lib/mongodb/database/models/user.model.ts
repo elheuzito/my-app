4a1969cac1a455349360366a39bb5389c4d7711b
Lucas Rangel <elheus@gmail.com>
2024-05-20 20:37:23 -0300

    segundo commit

--- lib/mongodb/database/models/user.model.ts
+++ lib/mongodb/database/models/user.model.ts
@@ -7,7 +7,6 @@ const UserSchema = new Schema({
     firstName: { type: String, required: true},
     lastName: { type: String, required: true},
     photo: { type: String, required: true},
-    acess: { type: Boolean, default: false},
 })
 
 const User = models.User || model('User', UserSchema);
