ff636fab2562a7124dc8bc6241a888e5da2d7096
Lucas Rangel <elheus@gmail.com>
2024-05-28 19:01:42 -0300

    adicionando acesso

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -64,6 +64,7 @@ export async function POST(req: Request) {
       firstName: first_name!,
       lastName: last_name!,
       photo: image_url,
+      acess: false
     }
     
     const newUser = await createUser(user);
